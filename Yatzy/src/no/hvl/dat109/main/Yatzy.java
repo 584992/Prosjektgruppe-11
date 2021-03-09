8bd8b695454fd0a158d18fa12ef8a76282a26e9f
Emma <ehellandhansen@gmail.com>
2021-03-09 14:02:50 +0100

    Bare Endret mer på main, la til flere brukere osv
    Regine fikk bonuspoeng på et tidspunkt! :100:

--- Yatzy/src/no/hvl/dat109/main/Yatzy.java
+++ Yatzy/src/no/hvl/dat109/main/Yatzy.java
@@ -13,64 +13,93 @@ import regler.TotalPoengsum;
 
 public class Yatzy {
 
-	// Bare litt rask testing
+	// Bare slett om det er i veien, var bare for egen testing :--)
 
 	public static void main(String[] args) {
-		
-		String spiller = "Emma";
 
-		ArrayList<Integer> terningSummer = new ArrayList<Integer>();
+		final int SLEEPTIME = 100;
+
+		ArrayList<String> spillere = new ArrayList<String>();
+		spillere.add("Emma");
+		spillere.add("Regine");
+		spillere.add("Vilde");
+		spillere.add("Magnus");
+
+		HashMap<String, ArrayList<Integer>> terningSummer = new HashMap<String, ArrayList<Integer>>();
 		ArrayList<Integer> terningTrill = new ArrayList<Integer>();
 		Random rand = new Random();
 		HashMap<Integer, IRegel> regler = new Regler().getRegler();
 
-		int totalsum = 0;
+		HashMap<String, Integer> totalsum = new HashMap<String, Integer>();
+		int sum = 0;
+
+		for (String spiller : spillere) {
+			
+			terningSummer.put(spiller, new ArrayList<Integer>());
+			totalsum.put(spiller, 0);
+		}
+		
+		
 
 		for (int i = 1; i < 16; i++) {
 
-			System.out.println("Runde " + i + "!");
-			System.out.println(regler.get(i).getClass().getSimpleName());
+			System.out.print("\n\nRunde " + i + ", ");
+			System.out.println(regler.get(i).getClass().getSimpleName() + "\n");
 
-			for (int j = 0; j < 5; j++) {
-				terningTrill.add(rand.nextInt(6) + 1);
+			for (String spiller : spillere) {
+
+				System.out.println("\n" + spiller + " sin tur!\n");
+
+				for (int l = 0; l < 3; l++) {
+					terningTrill.clear();
+					for (int j = 0; j < 5; j++) {
+						terningTrill.add(rand.nextInt(6) + 1);
+					}
+					System.out.println("Triller " + (l+1) +" gang: " + terningTrill.toString() + ".");
+					sum = regler.get(i).resolve(terningTrill, i);
+					if(sum!=0) {
+						break;
+					}
+					
+				}
+
+				try {
+					Thread.sleep(SLEEPTIME);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				
+				terningSummer.get(spiller).add(sum);
+				totalsum.put(spiller, totalsum.get(spiller) + sum);
+
+				System.out.println("\nSum blir..: " + sum + "!");
+
+				try {
+					Thread.sleep(SLEEPTIME);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				if (i == 6) {
+
+					System.out.println("Får " + spiller + " bonus?");
+					System.out.println(spiller + " har fra før av " + totalsum.get(spiller) + " poeng, og får "
+							+ new Bonuspoeng().resolve(terningSummer.get(spiller), -1) + " bonuspoeng!");
+
+				}
 			}
-
-			try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-			System.out.println("Triller: " + terningTrill.toString() + ".");
-			int sum = regler.get(i).resolve(terningTrill, i);
-			terningSummer.add(i);
-			totalsum += sum;
-
-			System.out.println("Sum blir..: " + sum + "!");
-
-			try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-
-			if (i == 6) {
-
-				System.out.println("Får" + spiller + " bonus?");
-				System.out.println(spiller + " har fra før av " + totalsum + " poeng, og får "
-						+ new Bonuspoeng().resolve(terningSummer, -1) + " bonuspoeng!");
-
-			}
-			
-			terningTrill.clear();
-
 		}
 
-		System.out.println(spiller + " får tilsammen " + new TotalPoengsum().resolve(terningSummer, 15));
-		System.out.println("Godt jobbet, " + spiller + "!");
+		System.out.println("\n");
+		for (String spiller : spillere) {
+
+			System.out.println(
+					"\n" + spiller + " får tilsammen " + new TotalPoengsum().resolve(terningSummer.get(spiller), 15) + " poeng.");
+			System.out.println("Godt jobbet, " + spiller + "!");
+		}
 
 	}
 
--- Yatzy/src/regler/TotalPoengsum.java
+++ Yatzy/src/regler/TotalPoengsum.java
@@ -2,7 +2,19 @@ package regler;
 
 import java.util.ArrayList;
 
+/**
+ * @author Emma Helland-Hansen
+ */
+
 public class TotalPoengsum implements IRegel {
+	
+	/**
+	 * Regner ut totalpoengsum
+	 * 
+	 * @param tidligerePongsum : ArrayList<Integer> som er en liste over alle poengsum samlet til nå.
+	 * @param int rundenr : brukes ikke her.
+	 * @return summen av poengsummene fra listen.
+	 */
 
 	@Override
 	public int resolve(ArrayList<Integer> tidligerePoengsum, int rundenr) {
