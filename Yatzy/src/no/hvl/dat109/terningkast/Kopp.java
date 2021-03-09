6c8ab64ca1048515ce0484c261fe357210265d1f
584992 <54098897+584992@users.noreply.github.com>
2021-03-09 11:14:34 +0100

    Oppdatering på Terning- og Kopp-klassen

--- Yatzy/src/no/hvl/dat109/terningkast/Kopp.java
+++ Yatzy/src/no/hvl/dat109/terningkast/Kopp.java
@@ -1,5 +1,49 @@
 package no.hvl.dat109.terningkast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 
+ * @author vilde
+ *
+ */
 public class Kopp {
+	
+	private Terning[] terninger = new Terning[5];
+	
+	/**
+	 * Triller terningene (5stk)
+	 * @param i er antall terninger du vil kaste
+	 */
+	public void trillerTerningene(int i) {
+		if ( i > 5 || i < 0) {
+		System.out.println("Du kan kun velge terninger mellom 1 og 5. Prøv på nytt.");	
+		} else {
+		int j = 0;
+		for (Terning terning : terninger) {
+			Terning terningKast = new Terning(terning.trill()); //Triller i terning-klassen
+			terninger[j] = terningKast;
+			i++;
+		}
+		}
+	}
+	
+	/**
+	 * Henter terningskastene i form av String
+	 * @return
+	 */
+	public String getTerningKast() {
+		String terningkast  = "";
+		int i = 0;
+		while (terninger[i]!=null) {
+			terningkast += "Terning " + (i+1) +  ": " + terninger[i];
+			i++;
+		}
+		
+		return terningkast;
+	}
+
 
 }
--- Yatzy/src/no/hvl/dat109/terningkast/Terning.java
+++ Yatzy/src/no/hvl/dat109/terningkast/Terning.java
@@ -1,5 +1,24 @@
 package no.hvl.dat109.terningkast;
 
 public class Terning {
+	
+private int tall;
+	
+	public Terning(int tall) {
+		this.tall = tall;
+	}
+	
+	public int getTall() {
+		return tall;
+	}
+
+	public void setTall(int tall) {
+		this.tall = tall;
+	}
+
+	public int trill() {
+		int tall = (int)(Math.random() *6)+1;
+		return tall;
+	}	
 
 }
